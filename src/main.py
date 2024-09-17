5f5feaf8901f86cb12d9a6beff955d8500a20391
brtalreja <110854777+brtalreja@users.noreply.github.com>
2024-09-16 23:58:45 -0500

    Descriptives

--- src/main.py
+++ src/main.py
@@ -5,3 +5,10 @@ import plotly.express as px
 #Loading the data
 data = pd.read_csv("../data/transaction_anomalies_dataset.csv")
 print(data.head())
+
+#Descriptives
+print(data.info())
+
+print(data.isnull().sum())
+
+print(data.describe())
