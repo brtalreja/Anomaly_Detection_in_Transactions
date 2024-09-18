e1a064ace0629d68ed47bf5bc08b79dfebd27849
brtalreja <110854777+brtalreja@users.noreply.github.com>
2024-09-17 23:42:57 -0500

    EDA start

--- src/main.py
+++ src/main.py
@@ -12,3 +12,21 @@ print(data.info())
 print(data.isnull().sum())
 
 print(data.describe())
+
+#EDA
+
+#Transaction Amount Distribution
+
+fig_amount = px.histogram(data,
+                          x = 'Transaction_Amount',
+                          nbins=20,
+                          title="Transaction Amount Distribution")
+
+fig_amount.show()
+
+fig_amount.write_image("../output/Transaction_Amount_Distribution.png")
+
+#COMMENTS:
+# 
+# 
+# 
