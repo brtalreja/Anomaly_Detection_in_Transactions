92d979e8b7f5af5f64b70762d657291de573e45c
brtalreja <110854777+brtalreja@users.noreply.github.com>
2024-09-17 23:51:09 -0500

    EDA start

--- src/main.py
+++ src/main.py
@@ -27,6 +27,9 @@ fig_amount.show()
 fig_amount.write_image("../output/Transaction_Amount_Distribution.png")
 
 #COMMENTS:
-# 
-# 
-# 
+# The plot shows a highly right-skewed distribution where the majority of transactions are concentrated between 850 and 1050.
+# There are a few outliers visible in the higher transaction range, specifically in the 2500–3000+ range which could represent anomalies and in turn indicate potential anomalies like fraud.
+# As the range of transactions is in between 850 and 1100, it can be a typical spending pattern or a standard transactional limit.
+
+#Transaction Amount vs Account Type
+
